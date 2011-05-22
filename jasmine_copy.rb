c6c7a6911609bf75f97951317a3603e03a724f36
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-22 10:04:20 -0400

    first working version. copies to current directory

--- jasmine_copy.rb
+++ jasmine_copy.rb
@@ -1,5 +1,21 @@
 # copies the dev/lib Selenium directories to current test/selenium directory
 #
 class Jasmine_Lib
-  @loc = '~/dev/lib/jasmine
+  attr_accessor :loc
+
+  def initialize
+    @loc = '~/dev/lib/jasmine'
+  end
 end
+
+class Command_Class
+  def copy_lib dir
+    `cp -R #{dir} .`
+  end
+end
+
+
+cmd = Command_Class.new
+lib = Jasmine_Lib.new
+
+cmd.copy_lib lib.loc
