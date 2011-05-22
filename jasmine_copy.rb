6ef94574577e346e301d1b7b9ad672a460921ab7
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-05-22 11:01:55 -0400

    removed getopt gem, can use getoptlong from stdlib instead

--- jasmine_copy.rb
+++ jasmine_copy.rb
@@ -1,5 +1,7 @@
 # copies the dev/lib Selenium directories to current test/selenium directory
 
+require File.dirname(__FILE__) + '/lib/jasmine_libs.rb'
+
 cmd = Command_Class.new
 lib = Jasmine_Lib.new
 
